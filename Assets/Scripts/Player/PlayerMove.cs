5227c466c117f3b431d2542cb20ffc6f2ed500a7
khaimaihoang <77196639+khaimaihoang@users.noreply.github.com>
2022-07-06 11:30:28 +0700

    Player move script

---
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    private float _inputVertical;
+    private float _inputHorizontal;
+
+    [SerializeField] private float _moveSpeed;
+    private float _moveSpeedMax;
+
+    [SerializeField] private bool _isTopdown;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        _moveSpeedMax = _moveSpeed;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        _inputVertical = Input.GetAxis("Vertical");
+        _inputHorizontal = Input.GetAxis("Horizontal");
+        if (Input.GetKeyDown(KeyCode.Space)) _isTopdown = !_isTopdown;
+    }
+
+    private void FixedUpdate()
+    {
+        if (_isTopdown)
+        {
+            transform.Translate(_inputHorizontal, _inputVertical, 0);
+        }
+        else
+        {
+            transform.Translate(_inputHorizontal, 0, _inputVertical);
+        }
+    }
+}
